22a6a6ff6eaac83a76c30ea331919ea0daec0f9d
Igor Grabarski <igorgrabarski@gmail.com>
2017-08-27 19:25:30 -0400

    Update db_connect.js

--- db_connect.js
+++ db_connect.js
@@ -1,10 +1,10 @@
 // ********** Connection to database ************
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'localhost',
+    host: 'YOUR_HOST',
     port: 3306,
-    user: 'root',
-    password: 'IgOr2708',
+    user: 'USERNAME',
+    password: 'PASSWORD',
     database: 'garagepay'
 });
 
